187e2b018fab209f4ae79aae47c633381b5df301
dark1922 <jvdematos004@gmail.com>
2021-10-08 19:03:01 -0300

    feath: método de trazer uma lista com jpa

---
+++ algafoods-api/src/main/java/com/algaworks/algafood/jpa/consultaCozinhaMain.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.jpa;
+
+import java.util.List;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.algaworks.algafood.AlgafoodsApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+
+public class consultaCozinhaMain {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodsApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		cadastroCozinha cadastroCozinhal = ((BeanFactory) applicationContext).getBean(cadastroCozinha.class);
+		
+		List<Cozinha> cozinhas = cadastroCozinhal.listar();
+		
+		for (Cozinha cozinha : cozinhas) {
+			System.out.println(cozinha.getNome());
+		}
+	}
+}
