cffd6833de5256abb9fda454917bb1129a661fc1
dark1922 <jvdematos004@gmail.com>
2021-10-12 19:10:31 -0300

    feath: buscar cozinha pelo id , usando xml e json.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,14 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
+	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE} )
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping("/{id}")
+	public Cozinha buscar(@PathVariable Long id) {
+		return cozinhaRepository.buscarId(id);
 	}
 
 }
