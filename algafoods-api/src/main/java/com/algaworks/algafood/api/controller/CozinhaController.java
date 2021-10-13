4111456746a737743a337378e6b26bc7f66b1c45
dark1922 <jvdematos004@gmail.com>
2021-10-13 16:45:06 -0300

    feath: tratando o buscar por id not found e ok.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,10 +3,14 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
@@ -31,8 +35,14 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{id}")
-	public Cozinha buscar(@PathVariable Long id) {
-		return cozinhaRepository.buscarId(id);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+		Cozinha cozinha = cozinhaRepository.buscarId(id);
+
+		if(cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 
 }
