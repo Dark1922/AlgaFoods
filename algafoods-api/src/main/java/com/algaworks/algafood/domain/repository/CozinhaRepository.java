b9e2137582de9ded10ab66db4f4d4fe7b8ab3a06
dark1922 <jvdematos004@gmail.com>
2021-10-19 18:28:28 -0300

    feath: Usando as keywords para definir critérios de query methods.

--- algafoods-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,13 +1,17 @@
 package com.algaworks.algafood.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
-  
-	List<Cozinha> nome(String nome);
+
 	
+	List<Cozinha> findTodasByNomeContaining(String nome);
+
+	Optional<Cozinha> findByNome(String nome);
+
 }
--- algafoods-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,8 +1,16 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+	
+	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+	
+	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+	
 }
