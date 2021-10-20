e6d916537eb7727fcfd126c07fa04fa7a733b5fa
dark1922 <jvdematos004@gmail.com>
2021-10-20 17:33:33 -0300

    feath: consulta dinâmica com criteria.

--- algafoods-api/src/main/java/com/algaworks/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood.domain.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -23,31 +27,33 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQuery {
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial , 
 			BigDecimal taxaFreteFinal ) {
 		
-		var jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
+		var builder = manager.getCriteriaBuilder(); //fábrica de de elementos pra consulta
 		
-		var parametros = new HashMap<String, Object>(); //mapa de string e objeto
+		var criteria = builder.createQuery(Restaurante.class); //construtor de clausulas
 		
-		if(StringUtils.hasLength(nome)) { //verifica se ele n está nulo nem vázio
-			jpql.append("and nome like: nome ");
-			parametros.put("nome","%" + nome + "%");
+		var root = criteria.from(Restaurante.class); //From restaurante
+		
+		var predicates = new ArrayList<Predicate>();
+		
+		if(StringUtils.hasText(nome)) {
+		predicates.add(builder.like(root.get("nome"), "%" +  nome + "%")); //consulta por partes de nome
 		}
+		
 		if(taxaFreteInicial != null) {
-			jpql.append("and  taxaFrete  >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
+		predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
 		}
+		
 		if(taxaFreteFinal != null) {
-			jpql.append("and  taxaFrete  <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);
+		predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
 		}
 		
+		criteria.where(predicates.toArray(new Predicate[0])); //instancia de array preenchido com todos predicates passados
 		
-		
-		TypedQuery<Restaurante> query = manager
-				.createQuery(jpql.toString(), Restaurante.class);
-		
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-		
+		var query = manager.createQuery(criteria);
 		return query.getResultList();
+		
+		
+		
+	
 	}
 }
