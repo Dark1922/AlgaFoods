bd6b8ab4c7a5a2ac84221acfdb275bc17d0bc809
dark1922 <jvdematos004@gmail.com>
2021-10-23 19:35:49 -0300

    feath: model produto , ManyToOne.

--- algafoods-api/src/main/java/com/algaworks/algafood/domain/model/Produto.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -1,15 +1,41 @@
 package com.algaworks.algafood.domain.model;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Produto {
 	
 	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+    
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private BigDecimal preco;
+	
+	@Column(nullable = false)
+	private Boolean ativo;
+	
+	@ManyToOne
+	@JoinColumn(nullable = true)
+	private Restaurante restaurante;
 }
