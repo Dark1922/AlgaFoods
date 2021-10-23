9874a33e05566ff703936b45dce665a58eaa3cb5
dark1922 <jvdematos004@gmail.com>
2021-10-23 15:51:45 -0300

    feath: Mapeando relacionamento bidirecional com @OneToMany.

--- algafoods-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,11 +1,16 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
@@ -26,4 +31,9 @@ public class Cozinha {
 	//@JsonProperty("titulo")
 	@Column(nullable = false)
 	private String nome;
+	
+	//faz um one to many aonde foi mapeado por cozinha no restaurante o manytoOne se chama cozinha e passa esse nome por mappedBy
+	@JsonIgnore //evitar loop
+	@OneToMany(mappedBy = "cozinha")  //Many é uma coção pq tem muitos ent tem o List
+	private List<Restaurante> restaurantes = new ArrayList<>();//instacia null por causa do nullpointerexception
 }
