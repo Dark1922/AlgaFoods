dd463c9fb871f0ac9cc6196b9ae81c8e4ebdc368
dark1922 <jvdematos004@gmail.com>
2021-11-04 20:10:40 -0300

    feath: tratamento pra url não mapeadas no nosso sistema.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,8 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
@@ -106,7 +109,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		Throwable rootCause = ExceptionUtils.getRootCause(ex); //cauxa raiz do erro na api
 		
 		if (rootCause instanceof InvalidFormatException) { //se for um formato invalido vai ser tratado aqui
-			return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
+			return handleInvalidFormat((InvalidFormatException) rootCause, headers, status, request);
 			
 		} else if (rootCause instanceof PropertyBindingException) {
 	        return handlePropertyBindingException((PropertyBindingException) rootCause, headers, status, request); 
@@ -125,7 +128,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	}
 
-	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex,
+	private ResponseEntity<Object> handleInvalidFormat(InvalidFormatException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
 		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
@@ -169,4 +172,60 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	        .map(ref -> ref.getFieldName())
 	        .collect(Collectors.joining("."));
 	}     
+	
+	/*
+	         Tratando exception de parâmetro de URL inválido
+	 
+	 1. MethodArgumentTypeMismatchException é um subtipo de TypeMismatchException
+
+	 2. ResponseEntityExceptionHandler já trata TypeMismatchException de forma mais abrangente
+	
+	 3. Então, especializamos o método handleTypeMismatch e verificamos se a exception
+	    é uma instância de MethodArgumentTypeMismatchException
+	
+	 4. Se for, chamamos um método especialista em tratar esse tipo de exception
+	
+	 5. Poderíamos fazer tudo dentro de handleTypeMismatch, mas preferi separar em outro método
+	 */
+	
+	@Override
+	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+	        HttpStatus status, WebRequest request) {
+	    
+	    if (ex instanceof MethodArgumentTypeMismatchException) {
+	        return handleMethodArgumentTypeMismatch(
+	                (MethodArgumentTypeMismatchException) ex, headers, status, request);
+	    }
+
+	    return super.handleTypeMismatch(ex, headers, status, request);
+	}
+
+	private ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+	        MethodArgumentTypeMismatchException ex, HttpHeaders headers,
+	        HttpStatus status, WebRequest request) {
+
+	    ProblemType problemType = ProblemType.PARAMETRO_INVALIDO;
+
+	    String detail = String.format("O parâmetro de URL '%s' recebeu o valor '%s', "
+	            + "que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
+	            ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+
+	    Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+	    return handleExceptionInternal(ex, problem, headers, status, request);
+	}
+	
+	/*tratamento pra url não mapeadas no nosso sistema*/
+	@Override
+	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, 
+	        HttpHeaders headers, HttpStatus status, WebRequest request) {
+	    
+	    ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+	    String detail = String.format("O recurso %s, que você tentou acessar, é inexistente.", 
+	            ex.getRequestURL());
+	    
+	    Problem problem = createProblemBuilder(status, problemType, detail).build();
+	    
+	    return handleExceptionInternal(ex, problem, headers, status, request);
+	}           
 }
--- algafoods-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -8,7 +8,9 @@ public enum ProblemType {
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada."),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso."),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio."),
-	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível.");
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível."),
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado");
 	
 	private String title;
 	private String uri;
