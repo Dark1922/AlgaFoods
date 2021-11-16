1b95d0989892292f8c6a1bc6f20ed075a8b1d1ae
dark1922 <jvdematos004@gmail.com>
2021-11-16 18:15:27 -0300

    feath 10.14. Limpando e populando o banco de dados de teste .

--- algafoods-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
+@Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
 	
--- algafoods-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ algafoods-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,12 +1,8 @@
 package com.algaworks.algafood;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
 
-import javax.validation.ConstraintViolationException;
-
-import org.flywaydb.core.Flyway;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,120 +11,76 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 
-import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
-import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.util.DatabaseCleaner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)//libera porta pra testes de api sem te que subir
-@TestPropertySource("/application-test.properties") //usa o properties-teste
-class CadastroCozinhaIT {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
+public class CadastroCozinhaIT {
 
-	@Autowired
-	private CadastroCozinhaService cadastroCozinha;
+	@LocalServerPort
+	private int port;
 	
 	@Autowired
-	private Flyway flyway;
+	private DatabaseCleaner databaseCleaner;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	@BeforeEach
-	public void setUp() { //vao ser reiniciado várias vezes em cada teste com as configuração daqui
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
 		
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); //se falhar mostra o log
-		RestAssured.port = port; //pega as porta aleatoria
-		RestAssured.basePath = "/cozinhas"; //nossa caminho de teste
-		
-		flyway.migrate(); //vai reiniciar o banco em cada teste
+		databaseCleaner.clearTables();
+		prepararDados();
 	}
 	
 	@Test
-	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
-		//cenário
-		Cozinha novaCozinha	= new Cozinha();
-		novaCozinha.setNome("Chinesa");
-		
-		//ação
-		novaCozinha = cadastroCozinha.adicionar(novaCozinha);
-		
-		//validação
-		assertThat(novaCozinha).isNotNull(); //validar q n retorna cozinha nula
-		assertThat(novaCozinha.getId()).isNotNull(); //tenha um id que não é nulo
-		
-	}
-	
-	@Test //podemos passar assim tb
-	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
-		Cozinha  novaCozinha = new Cozinha();
-		novaCozinha.setNome(null); 
-		
-		ConstraintViolationException erroEsperado =
-			      Assertions.assertThrows(ConstraintViolationException.class, () -> {
-			         cadastroCozinha.adicionar(novaCozinha);
-			      });
-			   
-			   assertThat(erroEsperado).isNotNull();
-	}
-	 
-	@Test
-	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
-		
-		EntidadeEmUsoException erroEsperado =
-			      Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
-			    	  cadastroCozinha.Excluir(1L);
-			      });
-			   
-			   assertThat(erroEsperado);
-		
-	} 
-
-	@Test 
-	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
-		
-		CozinhaNaoEncontradaException erroEsperado =
-			      Assertions.assertThrows(CozinhaNaoEncontradaException.class, () -> {
-			    	  cadastroCozinha.Excluir(100L);
-			      });
-			   
-			   assertThat(erroEsperado);
-	}
-	
-	/* Testes de API*/
-	
-	@LocalServerPort
-	private int port; //vai receber a porta que vai ser gerada pra subir o servidor
-	
-	@Test
-	public void deveRetornarStatus200_quandoConsultarCozinhas() {
-		
-		RestAssured.given() //dado que agente tem um path /cozinhas
-		.accept(ContentType.JSON) //quer do tipo json
-		   .when() //onde
-		.get() //no get oque está fornecendo pro when/ requisição
-		    .then() //oque
-		.statusCode(HttpStatus.OK.value()); //o status code tem que ser 200 ok
-	}
-	
-	@Test
-	public void deveConter4Cozinhas_QunadoConsultarCozinha() {
-		
-		RestAssured.given() //dado que agente tem um path /cozinhas
-		.accept(ContentType.JSON) //quer do tipo json
-		.when() //onde
-		   .get() //no get oque está fornecendo pro when/ requisição
-		.then() //oque
-		    .body("", Matchers.hasSize(4)); //tem q ter 4 objetos dentro do array de cozinhas cadastradas
-	}
-	
-	@Test
-	public void testRetornarStatus201_QuandoCadastrarCozinha() {
-		RestAssured.given()
-		.body("{\"nome\": \"Chinesa\"}")
-		.contentType(ContentType.JSON)
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		given()
+			.accept(ContentType.JSON)
 		.when()
-		.post()
+			.get()
 		.then()
-		.statusCode(HttpStatus.CREATED.value());
+			.statusCode(HttpStatus.OK.value());
 	}
+
+	@Test
+	public void deveConter2Cozinhas_QuandoConsultarCozinhas() {
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", hasSize(2));
+	}
+	
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		given()
+			.body("{ \"nome\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}
+	
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Tailandesa");
+		cozinhaRepository.save(cozinha1);
+
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Americana");
+		cozinhaRepository.save(cozinha2);
+	}
+	
 }
