4502d19ab7d0572066cec5487ec4968f05a40264
dark1922 <jvdematos004@gmail.com>
2021-11-21 18:02:19 -0300

    feath: Criando classes de mixin para usar as anotações do Jackson.

--- algafoods-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -28,8 +28,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.algaworks.algafood.core.validation.Groups;
 import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -54,7 +52,6 @@ public class Restaurante {
 	@Column(name = "taxa_frete" , nullable = false)
 	private BigDecimal taxaFrete;
 	
-	@JsonIgnoreProperties(value = "nome", allowGetters = true ) //qnd for serializar a cozinha ignore o nome de cozinha
 	@Valid //valid também a propriedade de cozinha / cascata
 	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class )
 	@NotNull
@@ -62,27 +59,22 @@ public class Restaurante {
 	@JoinColumn(nullable = false, name = "cozinha_id")
 	private Cozinha cozinha;
 	
-	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id")
 	,inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 	
-	@JsonIgnore
 	@Embedded //ela é uma parte da entidade restaurantes
 	private Endereco endereco;
 	
-	@JsonIgnore
 	@CreationTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataCadastro;
 	
-	@JsonIgnore
 	@UpdateTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;
 	
-	@JsonIgnore
 	@OneToMany(mappedBy = "restaurante")
 	private List<Produto> produtos = new ArrayList<>(); 
 }
