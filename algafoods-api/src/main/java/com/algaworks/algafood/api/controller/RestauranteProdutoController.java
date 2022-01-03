d1e428fbc4d3ee51319d9baa53ba3596342ed817
dark1922 <jvdematos004@gmail.com>
2022-01-03 12:08:24 -0300

    feath:  Implementando pesquisas simples na API.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,10 +46,17 @@ public class RestauranteProdutoController {
     private ProdutoInputDisassembler produtoInputDisassembler;
     
     @GetMapping
-    public List<ProdutoDTO> listar(@PathVariable Long restauranteId) {
+    public List<ProdutoDTO> listar(@PathVariable Long restauranteId, @RequestParam(required = false) boolean incluirInativos) {
         Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
         
-        List<Produto> todosProdutos = produtoRepository.findByRestaurante(restaurante);
+        List<Produto> todosProdutos = null;
+        
+        if(incluirInativos) {
+        	 todosProdutos = produtoRepository.findTodosByRestaurante(restaurante);
+        } else {
+        	
+        	 todosProdutos = produtoRepository.findAtivosByRestaurante(restaurante);
+        }
         
         return produtoModelAssembler.toCollectionModel(todosProdutos);
     }
