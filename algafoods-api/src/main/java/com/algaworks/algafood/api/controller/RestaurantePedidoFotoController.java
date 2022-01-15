e0b5e3c275115cf1b96117babbb23cb160f23ab0
dark1922 <jvdematos004@gmail.com>
2022-01-15 10:16:46 -0300

    feath: Desafio: Implementando endpoint de consulta de foto de produto.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/controller/RestaurantePedidoFotoController.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/controller/RestaurantePedidoFotoController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,5 +51,12 @@ public class RestaurantePedidoFotoController {
 		FotoProduto fotoSalva = catalogoFotoProduto.salvar(foto, arquivo.getInputStream());
 		return fotoProdutoModelAssembler.toModel(fotoSalva);
 		
+	}
+	@GetMapping
+	public FotoProdutoModel buscar(@PathVariable Long restauranteId, 
+	        @PathVariable Long produtoId) {
+	    FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
+	    
+	    return fotoProdutoModelAssembler.toModel(fotoProduto);
 	}
 }
--- algafoods-api/src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.algaworks.algafood.domain.exception.FotoProdutoNaoEncontradaException;
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
 import com.algaworks.algafood.domain.service.FotoStorageService.NovaFoto;
@@ -41,5 +42,10 @@ public class CatalogoFotoProdutoService {
 		 fotoStorageService.substituir(nomeArquivoExistente, novaFoto); 
 		 
 		 return foto;
+	}
+	
+	public FotoProduto buscarOuFalhar(Long restauranteId, Long produtoId) {
+	    return produtoRepository.findFotoById(restauranteId, produtoId)
+	            .orElseThrow(() -> new FotoProdutoNaoEncontradaException(restauranteId, produtoId));
 	}
 } 
