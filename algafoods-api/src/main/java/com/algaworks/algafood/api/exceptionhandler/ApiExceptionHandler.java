e59656d436f36e02762249d8bfc5bc42fbb12b6b
dark1922 <jvdematos004@gmail.com>
2022-01-17 10:47:55 -0300

    feath:Corrigindo exception handler de media type não aceita.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -16,6 +16,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -322,4 +323,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
 	}
 	
+	/*Corrigindo exception handler de media type não aceita/ pega o status e o header e retorna um responseentity sem precisar do body*/
+	//tirar a exception lançado do spring ao dar o 406
+	@Override
+	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return ResponseEntity.status(status).headers(headers).build();
+	}
 }
