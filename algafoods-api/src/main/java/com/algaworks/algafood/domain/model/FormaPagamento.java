cc62de2dfe7506ce30710e33593684188b76e9d1
dark1922 <jvdematos004@gmail.com>
2022-01-27 18:19:21 -0300

    feath: preparando a implementação de Deep ETags.

--- algafoods-api/src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.domain.model;
 
+import java.time.OffsetDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,4 +25,7 @@ public class FormaPagamento {
 
 	@Column(nullable = false)
 	private String descricao;
+	
+	@UpdateTimestamp //se que ouver uma atualização nesse entidade o jpa vai por aqui a data q foi atualizada
+	private OffsetDateTime dataAtualizacao;
 }
--- algafoods-api/src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -1,6 +1,9 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.time.OffsetDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.FormaPagamento;
@@ -8,4 +11,6 @@ import com.algaworks.algafood.domain.model.FormaPagamento;
 @Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
 
+	@Query("select max(dataAtualizacao) from FormaPagamento")
+	OffsetDateTime getDataUltimaAtualizacao();
 }
