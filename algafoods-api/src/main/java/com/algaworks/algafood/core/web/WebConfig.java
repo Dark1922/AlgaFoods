b807143d0e3b74ceba8a39afc0d13e40a8292a4b
dark1922 <jvdematos004@gmail.com>
2022-01-27 17:52:16 -0300

    feath:Implementando requisições condicionais com Shallow ETags.

--- algafoods-api/src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -1,6 +1,10 @@
 package com.algaworks.algafood.core.web;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +17,14 @@ public class WebConfig implements WebMvcConfigurer {//WebMvcConfigurer métodos 
 		.allowedMethods("*"); //permite todos métodos POST GET HEAD etc caso queira um em especifico só colocar só POST 
 		//.allowedOrigins("* astericos para todos ja vem como padrão ou uma url especifica https://algafoods.com.br")
 		//.maxAge(30) demora 30 segundos para fazer testes
+	}
+	
+	/*ao receber uma requisição, na hora que der uma resposta ele gera um hash dessa resposta e coloca o cabeçalho e tag
+	 *verifica se o hash dessa resposta coincide  com o Etag que está no if-none-match q está no cabeçalho q vem da requisição
+	 *se coincidir ele retorna 304 se n for igual ele continua deixa passar o processamento deixa a resposta chegar no servidor porem
+	 *ele adiciona o cabeçalho e tag*/
+	@Bean
+	public Filter  shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 }
