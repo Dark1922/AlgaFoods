15d1193191cc82b0b527d37e20ffc57338c5004e
dark1922 <jvdematos004@gmail.com>
2022-01-28 20:50:17 -0300

    feath: arrumando projeto para swager.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -73,12 +73,29 @@ import com.algaworks.algafood.domain.service.CadastroFormaPagamentoService;
 	    }
 	    
 	    @GetMapping("/{formaPagamentoId}")
-	    public ResponseEntity<FormaPagamentoDTO> buscar(@PathVariable Long formaPagamentoId) {
+	    public ResponseEntity<FormaPagamentoDTO> buscar(@PathVariable Long formaPagamentoId, ServletWebRequest request) {
+	    	
+	    	ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+	        
+	        String eTag = "0";
+	        
+	        OffsetDateTime dataAtualizacao = formaPagamentoRepository
+	                .getDataAtualizacaoById(formaPagamentoId);
+	        
+	        if (dataAtualizacao != null) {
+	            eTag = String.valueOf(dataAtualizacao.toEpochSecond());
+	        }
+	         
+	        if (request.checkNotModified(eTag)) {
+	            return null;
+	        }
+	    	
 	        FormaPagamento formaPagamento = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
 	        
 	        FormaPagamentoDTO formaPagamentoDTO = formaPagamentoModelAssembler.toModel(formaPagamento);
 	        return ResponseEntity.ok()
 	        		.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+	        		.eTag(eTag)
 	        		.body(formaPagamentoDTO);
 	    }
 	    
