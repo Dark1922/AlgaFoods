1e14148860786fec5db06b1a04619e657d2068e0
dark1922 <jvdematos004@gmail.com>
2022-01-29 16:27:43 -0300

    feath: Descrevendo informações da API na documentação.

--- algafoods-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,12 +25,22 @@ public class SpringFoxConfig  implements WebMvcConfigurer  {
 				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api")) //tudo que tiver no projeto pode colocar , os endpoint que quer documentar
 				.paths(PathSelectors.any()) //já fica por padrão
 				//.paths(PathSelectors.ant("/restaurantes/*"))
-				.build();
+				.build()
+				.apiInfo(apiInfo());
 	}
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	    registry.addResourceHandler("index.html")
 	    	.addResourceLocations("classpath:/META-INF/resources/");
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Algafood API")
+				.description("API aberta para clientes e restaurantes")
+				.version("1")
+				.contact(new Contact("Algaworks", "https://www.algaworks.com", "contato@algaworks.com"))
+				.build();
 	}
 
 }
