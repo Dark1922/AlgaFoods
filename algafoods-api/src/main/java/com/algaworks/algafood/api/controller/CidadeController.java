5f90811182e5e3e291ed36ab915f91f872c8d540
dark1922 <jvdematos004@gmail.com>
2022-01-30 14:32:40 -0300

    feath: Descrevendo parâmetros de entrada na documentação.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -28,6 +28,7 @@ import com.algaworks.algafood.domain.service.CadastroCidadeService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Api(tags = "Cidades")
 @RestController
@@ -39,10 +40,10 @@ public class CidadeController {
 
 	@Autowired
 	private CadastroCidadeService cadastrocidadeService;
-	
+
 	@Autowired
 	private CidadeModelAssembler cidadeModelAssembler;
-	
+
 	@Autowired
 	private CidadeInputDisassembler cidadeInputDisassembler;
 
@@ -51,17 +52,21 @@ public class CidadeController {
 	public List<CidadeDTO> listar() {
 		return cidadeModelAssembler.toCollectionModel(cidadeRepository.findAll());
 	}
+
 	@ApiOperation("Buscar uma cidade por ID")
 	@GetMapping("/{cidadeId}")
-	public CidadeDTO buscar(@PathVariable Long cidadeId) {
+	public CidadeDTO buscar(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long cidadeId) {
 		Cidade cidade = cadastrocidadeService.buscarOuFalhar(cidadeId);
 		return cidadeModelAssembler.toModel(cidade);
 	}
-     
+
 	@ApiOperation("Cadastra uma Cidade")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public CidadeDTO adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
+	public CidadeDTO adicionar(
+			@ApiParam(name = "corpo", value = "Representação de uma nova cidade com os novos dados") 
+			@RequestBody @Valid CidadeInput cidadeInput) {
+
 		try {
 			Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
 			return cidadeModelAssembler.toModel(cadastrocidadeService.salvar(cidade));
@@ -69,17 +74,19 @@ public class CidadeController {
 			throw new NegocioException(e.getMessage(), e);
 		}
 	}
-    
+
 	@ApiOperation("Atualiza uma cidade por ID")
 	@PutMapping("/{cidadeId}")
-	public CidadeDTO atualizar(@PathVariable Long cidadeId, @RequestBody @Valid CidadeInput cidadeInput) {
+	public CidadeDTO atualizar(
+			@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long cidadeId,
+			@ApiParam(name = "corpo", value = "Representação de uma nova cidade com os novos dados") 
+			@RequestBody @Valid CidadeInput cidadeInput) {
 
 		try {
 			Cidade cidadeAtual = cadastrocidadeService.buscarOuFalhar(cidadeId);
 			cidadeInputDisassembler.copyToDomainObject(cidadeInput, cidadeAtual);
-			
+
 			return cidadeModelAssembler.toModel(cadastrocidadeService.salvar(cidadeAtual));
-			
 
 		} catch (EstadoNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage(), e);
@@ -89,10 +96,8 @@ public class CidadeController {
 	@ApiOperation("Exclui uma cidade por ID")
 	@DeleteMapping("/{cidadeId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long cidadeId) {
+	public void remover(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long cidadeId) {
 		cadastrocidadeService.excluir(cidadeId);
 	}
-	
-
 
 }
