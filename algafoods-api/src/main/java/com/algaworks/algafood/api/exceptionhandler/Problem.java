9d2191d321d35ab6311455f4e1bcda4597f8c027
dark1922 <jvdematos004@gmail.com>
2022-01-30 17:23:36 -0300

    feath: Descrevendo códigos de status de respostas de forma global.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -7,28 +7,48 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+@ApiModel("Problema")
 @Getter
 @Builder //linguagem mais fluente
 @JsonInclude(Include.NON_NULL) //só inclua na propriação json se não for nulo
 public class Problem {
 
+	@ApiModelProperty(example = "400", position = 1)
 	private Integer status;
+	
+	@ApiModelProperty(example = "https://algafood.com.br/erro-negocio", position = 10)
     private String type;
+	
+	@ApiModelProperty(example = "Violação de regra de negócio",position = 20)
     private String title;
+	
+	@ApiModelProperty(example = "Não existe um cadastro de estado com codigo 1",position = 30)
     private String detail;
+	
+	@ApiModelProperty(example = "Não existe um cadastro de estado com codigo 1",position = 35)
     private String userMessage;
-    private List<Object> objects;
+	
+	@ApiModelProperty(value="Objetos ou campos que geraram o erro",position = 50)
+	private List<Object> objects;
     
+	@ApiModel("ObjetoProblema")
     @Getter
     @Builder
     public static class Object {
+		
+		@ApiModelProperty(example = "Estado")
     	private String name;
+		
+		@ApiModelProperty(example = "Estado não encontrado")
     	private String userMessage;
     }
 	
+	@ApiModelProperty(example = "2021-11-14T00:12:13",position = 40)
 	@JsonFormat(timezone = "GMT-3",pattern = "yyyy-MM-dd hh:mm:ss")
 	private OffsetDateTime timestamp; // do acontecimento do erro
 	
--- algafoods-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,17 +1,27 @@
 package com.algaworks.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -30,13 +40,46 @@ public class SpringFoxConfig  implements WebMvcConfigurer  {
 				.paths(PathSelectors.any()) //já fica por padrão
 				//.paths(PathSelectors.ant("/restaurantes/*"))
 				.build()
+				.useDefaultResponseMessages(false)//deixa como false as mensagem de erro que o swagger preve pra nós
+				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades","Gerencia as cidades"));
 	}
+	
+	private List<Response> globalGetResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+						.code(comoString(HttpStatus.INTERNAL_SERVER_ERROR))
+						.description("Erro interno do Servidor")
+						.build(),
+				new ResponseBuilder()
+						.code(comoString(HttpStatus.NOT_ACCEPTABLE))
+						.description("Recurso não possui representação que pode ser aceita pelo consumidor")
+						.build()
+		);
+	}
+	
+	private String comoString(HttpStatus httpStatus) {
+		return String.valueOf(httpStatus.value());
+	}
+	@SuppressWarnings("unused")
+	private Consumer<RepresentationBuilder> problemBuilder() {
+		return r -> r.model(m -> m.name("Problema")
+				.referenceModel(
+						ref -> ref.key(
+								k -> k.qualifiedModelName(
+										q -> q.name("Problema")
+											  .namespace("com.algaworks.algafood.api.exceptionhandler")
+								))));
+	}
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	    registry.addResourceHandler("index.html")
 	    	.addResourceLocations("classpath:/META-INF/resources/");
+	    
+	    registry.addResourceHandler("/webjars/**")
+		.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 	
 	public ApiInfo apiInfo() {
