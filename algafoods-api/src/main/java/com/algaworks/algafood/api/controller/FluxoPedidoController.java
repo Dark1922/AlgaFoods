11b042f1dfab7aa95a76d7e6ecd54569929a70f8
dark1922 <jvdematos004@gmail.com>
2022-02-02 12:41:51 -0300

    feath: Desafio: descrevendo documentação de endpoints de fluxo de pedidos.

--- algafoods-api/src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
@@ -2,17 +2,19 @@ package com.algaworks.algafood.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.openapi.controller.FluxoPedidoControllerOpenApi;
 import com.algaworks.algafood.domain.service.FluxoPedidoService;
 
 @RestController
-@RequestMapping(value = "/pedidos/{codigoPedido}")
-public class FluxoPedidoController {
+@RequestMapping(path = "/pedidos/{codigoPedido}", produces =  MediaType.APPLICATION_JSON_VALUE)
+public class FluxoPedidoController implements FluxoPedidoControllerOpenApi{
 
 	@Autowired
 	private FluxoPedidoService fluxoPedidoService;
