277470847ee85bccc8727399d8476b4950c2e180
dark1922 <jvdematos004@gmail.com>
2022-02-16 12:52:44 -0300

    feath:Configurando autenticação de usuários em memória.

--- algafoods-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -1,12 +1,25 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity //ativa a segurança e tem o configuration tb
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		  .withUser("admin")
+		  .password(passwordEncoder().encode("admin"))
+		  .roles("ADMIN");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -23,5 +36,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		      .and()
 		         .csrf().disable(); //só precisa qnd usa cookie ent desativa pq vamos passas as credencias e usar auth02
 	}
-
+	
+	@Bean  //criptografando a senha usando bcrypt
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+      
+	
 }
