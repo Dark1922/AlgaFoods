c5ccebba72fffc4c2a8b301ad95e12f6cb961615
dark1922 <jvdematos004@gmail.com>
2022-02-16 12:42:42 -0300

    feath: Configurando Spring Security com HTTP Basic.

---
+++ algafoods-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.core.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity //ativa a segurança e tem o configuration tb
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+		.and()
+		.authorizeRequests()
+		  .antMatchers("/v1/cozinhas/**").permitAll() //permite tudo antes de restringir autenticated
+		  .anyRequest().authenticated() //autoriza requisição qualquer requisição autenticada
+		  
+		  .and()
+		    .sessionManagement()
+		      .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //n vai usar sessions tira os cookies
+		      
+		      .and()
+		         .csrf().disable(); //só precisa qnd usa cookie ent desativa pq vamos passas as credencias e usar auth02
+	}
+
+}
