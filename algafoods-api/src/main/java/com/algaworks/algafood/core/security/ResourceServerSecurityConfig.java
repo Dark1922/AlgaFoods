270999c1afd5e1c45a6c8cd07e125da946a9b693
dark1922 <jvdematos004@gmail.com>
2022-03-04 20:42:54 -0300

    feath: colocando jwt.

--- algafoods-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerSecurityConfig.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerSecurityConfig.java
@@ -23,7 +23,7 @@ public class ResourceServerSecurityConfig extends WebSecurityConfigurerAdapter {
 		  .cors().and() //libera o cors
 		  .oauth2ResourceServer().jwt(); //configurando um resource server
 	}
-	
+	 
 	@Bean
 	public JwtDecoder jwtDecoder() {
 		var secretKey = new SecretKeySpec("DB4AEF4719809709E560ED8DE2F9C77B886B963B28BA20E9A8A621BBD4ABA400".getBytes(),
