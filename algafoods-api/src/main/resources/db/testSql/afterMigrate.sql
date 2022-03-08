3a758787fe5164448bd442f532dd7a0ca45551dc
dark1922 <jvdematos004@gmail.com>
2022-03-08 19:35:54 -0300

    feath:Carregando as permissões concedidas na autenticação.

--- algafoods-api/src/main/resources/db/testSql/afterMigrate.sql
+++ algafoods-api/src/main/resources/db/testSql/afterMigrate.sql
@@ -122,7 +122,7 @@ insert into usuario (id, nome, email, senha, data_cadastro) values
 (6, 'Débora Mendonça', 'debora_mendonça@gmail.com', '$2a$12$DIM.mJAaGWAhnvMgkdRAo.nzyzKPqRELin7yr6XpsZMY0mx9LEA5C', utc_timestamp),
 (7, 'Carlos Lima', 'email.teste.aw+carlos@gmail.com', '$2a$12$DIM.mJAaGWAhnvMgkdRAo.nzyzKPqRELin7yr6XpsZMY0mx9LEA5C', utc_timestamp);
 
-insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
+insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2),(3, 3), (4, 4);
 
 insert into restaurante_usuario_responsavel (restaurante_id, usuario_id) values (1, 5), (3, 5);
 
