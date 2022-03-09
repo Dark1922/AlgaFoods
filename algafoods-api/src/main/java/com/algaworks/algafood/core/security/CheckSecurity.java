fed4db4d627fff05332971c25acc877f6efd79b5
dark1922 <jvdematos004@gmail.com>
2022-03-09 19:53:45 -0300

    feath: Restringindo acesso a endpoints por escopos do OAuth2.

--- algafoods-api/src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -12,12 +12,12 @@ public @interface CheckSecurity {
     
 	public @interface Cozinhas {
 		
-		@PreAuthorize("isAuthenticated()")
+		@PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
 		@Retention(RUNTIME)
 		@Target(METHOD)
 		public @interface PodeConsultar {}
 		
-		@PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
+		@PreAuthorize("hasAuthority('SCOPE_WRITE') and hasAuthority('EDITAR_COZINHAS')")
 		@Retention(RUNTIME)
 		@Target(METHOD)
 		public @interface PodeEditar {}
