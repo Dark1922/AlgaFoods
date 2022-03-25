f21c9b919db23482c1354ea07fcbc14f1e0cdf0f
dark1922 <jvdematos004@gmail.com>
2022-03-25 19:37:41 -0300

    feath: organizando a documentação;

--- algafoods-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
+++ algafoods-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -60,7 +60,7 @@ public interface RestauranteProdutoFotoControllerOpenApi {
             @ApiParam(value = "ID do produto", example = "1", required = true)
             Long produtoId);
 
-    
+     
     
     @ApiOperation(value = "Busca a foto do produto de um restaurante", hidden = true)//hidden tru ocultada na documentação esse endpoint
     ResponseEntity<?> servirFoto(Long restauranteId, Long produtoId, String acceptHeader) 
